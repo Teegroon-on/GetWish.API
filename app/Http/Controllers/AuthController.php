0ceb001e0b1849951e054ae46db3453c7652bd0b
epxch <snigir_00@mail.ru>
2023-05-17 20:33:11 +0300

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
      *
      */
     public function send(SmsCodeSendRequest $request, SmsAeroApi $sms) {
-        return reponse() -> json("huy");
+        return response() -> json("huy");
         $validDate = now()->subMinute();
         $smsCode = SmsCode::where('phone', $request->phone)
             ->where('used', false)
