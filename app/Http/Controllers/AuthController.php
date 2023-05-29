f7272ed79545c1f5659d74e3dcfa7f2a8a6e8553
epxch <snigir_00@mail.ru>
2023-05-30 00:39:12 +0300

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,8 +37,8 @@ class AuthController extends Controller
             $smsCode->code = $generatedCode;
             $sms->send($request->phone, 'Ваш код для входа: '.$generatedCode);
         }
-        $smsCode->save();
         return response() -> json('huy');
+        $smsCode->save();
         return response()->json('', 200);
     }
 
