d6ce4d5eadce43419e0fb0b4831570aea486798c
epxch <snigir_00@mail.ru>
2023-05-30 16:14:08 +0300

    aasd

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -113,9 +113,6 @@ class PostController extends Controller
                 $newPost->delete();
                 return response()->json('This file cannot be added', 461);
             }
-            $response = [];
-            $response['id'] = $newPost->id;
-            return $response;
             return new PostUploadAttachmentResource($newPost);
         } else {
             $post = Post::find(intval($id));
--- app/Http/Resources/PostUploadAttachmentResource.php
+++ app/Http/Resources/PostUploadAttachmentResource.php
@@ -14,6 +14,7 @@ class PostUploadAttachmentResource extends JsonResource
      */
     public function toArray($request)
     {
+        return 'huy';
         $response = [];
         $response['id'] = $this->id;
         return $response;
